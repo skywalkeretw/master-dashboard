54856690b8209126e2c9e7865e89e1bdb8030d39
Luke Roy <lukeroy@outlook.com>
2024-01-04 18:29:23 +0100

    add Svelte UI and layout page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { SvelteUIProvider } from '@svelteuidev/core';
+	import HeadContent from './headContent.svelte';
+</script>
+
+<SvelteUIProvider withGlobalStyles>
+	<HeadContent/>
+	<slot />
+</SvelteUIProvider>
---
+++ src/routes/headContent.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { colorScheme, Stack, Title, Button, Switch } from '@svelteuidev/core';
+
+    function toggleTheme() {
+       colorScheme.update((v) => (v === 'light' ? 'dark' : 'light'));
+   }
+</script>
+
+<style>
+    .stack {
+        padding: 2rem;
+        display: flex;
+        justify-content: space-between; /* Align items to the left and right */
+        align-items: center;
+    }
+</style>
+
+<div class="stack">
+    <Title order={1}>Functions</Title>
+    <Stack>
+        <!-- Use the Button component for the button -->
+        <!-- <Button on:click={() =>alert("ok")}>Button</Button> -->
+        <Switch 
+            size="lg"
+            radius="sm"
+            color="dark"
+            on:change={toggleTheme}
+        />
+    </Stack>
+</div>
